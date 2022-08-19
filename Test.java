54fa12f931578a3edb8d8c9ce7e97381c79b5168
lockie <948656374@qq.com>
2022-08-19 22:40:07 +0800

    test

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public int hashCode1() {
+        String xyt = "111111111111111111111111111111111111111111111";
+        return super.hashCode();
+    }
+}
