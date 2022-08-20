b8884eb7e6ad8b76a281ca309414ba9c3a2ee5b3
lockieprime <49502901+lockieprime@users.noreply.github.com>
2022-08-20 19:04:24 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,7 +9,7 @@ public class Test {
     public int hashCode1() {
         int c = 3;
         int a = 2;
-        int b = a + c;
+        int b = a + c + 5;
 
         // 我提交了。别人提交的代码。我提交的代码。
         return super.hashCode();
